1de84ba0591c7a1859fdd774fbc3edfbd574957c
lester0866 <sli96@u.rochester.edu>
2018-07-11 22:05:50 +0800

    delte

--- myPrograms/pages/index/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    Bucket: 'userdata-1257097340',//replace with yours
-    Region: 'ap-beijing',//replace with yours
-    SecretId: 'AKIDFMH0r55dgXDi1CyxYxACD6veCYih3BjL',//replace with yours
-    SecretKey:'c2GLE3kFYfeU8aw64va3Yr03vb6PZagg'//replace with yours
-}
-
-module.exports = config
